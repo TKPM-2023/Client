9675362d608ce7ddc014e86d35104925a4aa2aea
20120583 <trananhthi56@gmail.com>
2023-06-15 12:10:10 +0700

    Update home page: display categories and products

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,46 @@
 import { Helmet } from 'react-helmet-async'
 import { Typography } from '@material-tailwind/react'
+import { useQuery } from '@tanstack/react-query'
+
 import HomeCarousel from './components/HomeCarousel'
 import HomeCategory from './components/HomeCategory'
 import ListProduct from './components/ListProduct'
+import status from 'src/constants/status'
+
+import categoryApi from 'src/apis/category.api'
+import productApi from 'src/apis/product.api'
+import { Category, CategoryListConfig } from 'src/types/category.type'
+import { Product, ProductListConfig } from 'src/types/product.type'
 
 function Home() {
+  const categoryQueryConfig: CategoryListConfig = {
+    limit: '10'
+  }
+  const productQueryConfig: ProductListConfig = {
+    status: String(status.inStore),
+    limit: '12'
+  }
+
+  const { data: categoryData, refetch: refetchCategory } = useQuery({
+    queryKey: ['categories', categoryQueryConfig],
+    queryFn: () => categoryApi.getCategories(categoryQueryConfig),
+    keepPreviousData: true
+  })
+
+  const { data: productData, refetch: refetchProduct } = useQuery({
+    queryKey: ['products', productQueryConfig],
+    queryFn: () => productApi.getProducts(productQueryConfig),
+    keepPreviousData: true
+  })
+
+  const categories = categoryData?.data.data
+  const products = productData?.data.data
+
+  const handlefecth = () => {
+    refetchCategory()
+    refetchProduct()
+  }
+
   return (
     <>
       <div className='h-full bg-gray-100 py-10'>
@@ -17,7 +53,7 @@ function Home() {
             <HomeCarousel />
           </div>
           <div>
-            <HomeCategory />
+            <HomeCategory categories={categories as Category[]} />
           </div>
           <div className='mb-4 text-center'>
             <Typography color='blue-gray' variant='h3' className='font-bold'>
@@ -25,7 +61,7 @@ function Home() {
             </Typography>
           </div>
           <div>
-            <ListProduct />
+            <ListProduct products={products as Product[]} />
           </div>
         </div>
       </div>
